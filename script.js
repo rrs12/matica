a9c54d3ec854898d92db93f397ba13936173288f
Vedant Agnihotri <84136631+VedantAgnihotri@users.noreply.github.com>
2023-09-29 18:35:39 +0530

    side-opening navbar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function opennav() {
-  document.getElementById("navbar-default").classList.toggle("hidden");
-}
+const navLinks = document.querySelector('.nav-links')
+        function onToggleMenu(e){
+            e.name = e.name === 'menu' ? 'close' : 'menu'
+            navLinks.classList.toggle('left-[60%]')
+            navLinks.classList.toggle('shadow')
+            navLinks.classList.toggle('text-cyan-400')
+            navLinks.classList.toggle('bg-sky-900')
+        }
