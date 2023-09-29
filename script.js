d62cb539d5b27ed0b29734957afda06f4ea877b2
rrs12 <rahilsalecha12@gmail.com>
2023-09-29 18:57:16 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const navLinks = document.querySelector('.nav-links')
         function onToggleMenu(e){
             e.name = e.name === 'menu' ? 'close' : 'menu'
-            navLinks.classList.toggle('left-[60%]')
+            navLinks.classList.toggle('left-[20%]')
             navLinks.classList.toggle('shadow')
-            navLinks.classList.toggle('text-cyan-400')
-            navLinks.classList.toggle('bg-sky-900')
+            navLinks.classList.toggle('nav_bar')
         }
