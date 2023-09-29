8cd06fc1269577bc9ab00500c76190c04fa7ae26
Vedant Agnihotri <84136631+VedantAgnihotri@users.noreply.github.com>
2023-09-29 22:12:48 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const navLinks = document.querySelector('.nav-links')
+const logo_text = document.querySelector('.logo_text')
+const menu_button = document.querySelector('.menu_button')
         function onToggleMenu(e){
             e.name = e.name === 'menu' ? 'close' : 'menu'
             navLinks.classList.toggle('left-[10%]')
             navLinks.classList.toggle('shadow')
             navLinks.classList.toggle('nav_bar')
             navLinks.classList.toggle('bg-gray-900')
+            logo_text.classList.toggle('hidden')
+            /**menu_button.classList.toggle('hidden')**/
         }
