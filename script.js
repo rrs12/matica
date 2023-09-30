f78071a0c6f6279c7162b5b0b3ea0c48dce937e6
Vedant Agnihotri <84136631+VedantAgnihotri@users.noreply.github.com>
2023-09-30 13:51:18 +0530

    Final update
    fix 1: Desktop-view nav elements
    fix 2: Mobile-view navbar Bell Icon, Profile Icon, Theme Icon should be on top

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const navLinks = document.querySelector('.nav-links')
 const logo_text = document.querySelector('.logo_text')
 const menu_button = document.querySelector('.menu_button')
+const home_section = document.querySelector('.home_section')
         function onToggleMenu(e){
             e.name = e.name === 'menu' ? 'arrow-forward' : 'menu'
             navLinks.classList.toggle('left-[10%]')
             navLinks.classList.toggle('shadow')
             navLinks.classList.toggle('nav_bar')
             navLinks.classList.toggle('bg-gray-900')
-            navLinks.classList.toggle('shadow')
+            navLinks.classList.toggle('shadow-2xl')
             logo_text.classList.toggle('hidden')
             menu_button.classList.toggle('bg-gradient')
+            home_section.classList.toggle('blur')
         }
 const theme_btn = document.querySelector('.theme_btn')
           function theme_change(e){
