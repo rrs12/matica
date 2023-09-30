2a16d43449ced9518d69f4545eb30555fa3704c1
Vedant Agnihotri <84136631+VedantAgnihotri@users.noreply.github.com>
2023-09-30 13:11:49 +0530

    update

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const navLinks = document.querySelector('.nav-links')
 const logo_text = document.querySelector('.logo_text')
 const menu_button = document.querySelector('.menu_button')
         function onToggleMenu(e){
-            e.name = e.name === 'menu' ? 'close' : 'menu'
+            e.name = e.name === 'menu' ? 'arrow-forward' : 'menu'
             navLinks.classList.toggle('left-[10%]')
             navLinks.classList.toggle('shadow')
             navLinks.classList.toggle('nav_bar')
             navLinks.classList.toggle('bg-gray-900')
-            navLinks.classList.toggle('shadow-lg')
+            navLinks.classList.toggle('shadow')
             logo_text.classList.toggle('hidden')
             menu_button.classList.toggle('bg-gradient')
+        }
+const theme_btn = document.querySelector('.theme_btn')
+          function theme_change(e){
+            e.name = e.name === 'sunny' ? 'moon' : 'sunny'
+            theme_btn.classList.toggle('text-gray-50')
         }
