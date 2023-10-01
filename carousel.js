0fc2900386a7f49d4c41d1ecc29b97496b767030
rrs12 <rahilsalecha12@gmail.com>
2023-10-01 21:29:11 +0530

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,32 +1,22 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items: 4,
-    // items change number for slider display on desktop
-
+$('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
+    nav: false,
+    dots: false,
+    stagePadding: 50,
     autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true
-});
-
-+$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    responsive : {
+        0 : {
+            items : 1
+           
         },
-        600:{
-            items:3,
-            nav:false
+        917 : {
+            items : 2
+           
         },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
+        1100 : {
+            items : 3
         }
-    }
-})
+    }})
