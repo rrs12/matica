ac358301bcc582a08ec2209721cdeac39dab1188
rrs12 <rahilsalecha12@gmail.com>
2023-10-01 19:35:53 +0530

    push

--- carousel.js
+++ carousel.js
@@ -9,3 +9,24 @@ owl.owlCarousel({
     autoplayTimeout: 3000,
     autoplayHoverPause: true
 });
+
++$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
--- script.js
+++ script.js
@@ -28,23 +28,8 @@ var typed = new Typed(".typing", {
     backSpeed: 80,
     loop: true
 });
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
+
+function hide(){
+    home_section.classList.toggle('hidden')
+    features_section.classList.toggle('hidden')
+    main_navbar.classList.toggle('hidden')}
