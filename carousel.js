a443cc68688d9ed484a9e159d15bcba86acc20dc
rrs12 <rahilsalecha12@gmail.com>
2023-10-02 17:53:36 +0530

    s

--- carousel.js
+++ carousel.js
@@ -1,20 +1,20 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
-    margin: 10,
+    margin: 5,
     nav: false,
     dots: false,
     stagePadding: 50,
     autoplay: true,
-    autoplayTimeout: 1000,
+    autoplayTimeout: 2500,
     autoplayHoverPause: true,
     responsive : {
         0 : {
             items : 1
-           
+
         },
         917 : {
             items : 2
-           
+
         },
         1100 : {
             items : 3
